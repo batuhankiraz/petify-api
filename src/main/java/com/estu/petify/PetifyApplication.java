3b746879032b11c3e8bab28164f3c80f124524eb
batuhankiraz <batuhankiraz@eskisehir.edu.tr>
2021-11-07 16:39:28 +0300

    initial commit for petify web application.

---
+++ src/main/java/com/estu/petify/PetifyApplication.java
@@ -0,0 +1,13 @@
+package com.estu.petify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estu/petify/PetifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estu.petify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
